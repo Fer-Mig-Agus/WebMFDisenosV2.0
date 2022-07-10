66c3a702f8550f00aa00e65fdc3ce27641e282ff
Fer-Mig-Agus <miguelfernandez.m.f.8998@gmail.com>
2022-07-10 12:19:30 -0300

    No pude solucionar lo anterior asi que lo pase a una nueva carpeta

---
+++ js/app.js
@@ -0,0 +1,13 @@
+const fullImgBox=document.getElementById("fullImgBox");
+const fullimg=document.getElementById("fullimg");
+
+function openFullImg(reference){
+    fullImgBox.style.display="flex";
+    fullimg.src=reference;
+}
+
+function closeImg(){
+    fullImgBox.style.display="none";
+
+}
+
