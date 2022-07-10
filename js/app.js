2a7ee77b1c8e6c4f6a54eb0ba1509d7f961e83f6
Fer-Mig-Agus <miguelfernandez.m.f.8998@gmail.com>
2022-07-10 19:03:33 -0300

    JS: cambie el tamaño de las imagenes al abrirla para que se ajuste al ancho que deberia, ademas de que hice funcional el boton de ver mas

--- js/app.js
+++ js/app.js
@@ -8,8 +8,9 @@ function openFullImg(reference,valor){
     }else{
         fullImgBox.style.display="flex";
         fullimg.src=reference;
-        fullimg.style.width=400;
-        fullimg.style.height=auto;
+        fullimg.style.width='500px';
+        fullimg.style.marginLeft='35%';
+        
     }
     
 }
@@ -17,5 +18,27 @@ function openFullImg(reference,valor){
 function closeImg(){
     fullImgBox.style.display="none";
 
+}
+
+const fullImgVert=document.querySelector('.bottom-verMas');
+const masInfo=document.querySelector('#mas-info-tarjetas');
+const divMore=document.querySelector('#imgMore');
+
+fullImgVert.addEventListener('click',funcionVerMas);
+
+console.log(divMore.style.display.contains());
+
+function funcionVerMas(){
+    if(divMore.classList.contains('activo')){
+        divMore.classList.remove('activo');
+        divMore.style.display='none';
+        masInfo.style.display='none';
+        this.textContent='Ver Mas...'
+    }else{
+        divMore.classList.add('activo');
+        divMore.style.display='block';
+        masInfo.style.display='block';
+        this.textContent='Ver Menos...'
+    }
 }
 
