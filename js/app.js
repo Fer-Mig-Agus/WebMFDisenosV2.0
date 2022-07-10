a30d0d17e7c4c3d6ae71d825d79849497d0e9441
Fer-Mig-Agus <miguelfernandez.m.f.8998@gmail.com>
2022-07-10 13:17:04 -0300

    JS: hice un cambio en la condicion para abrir la imagen vertical

--- js/app.js
+++ js/app.js
@@ -1,9 +1,17 @@
 const fullImgBox=document.getElementById("fullImgBox");
 const fullimg=document.getElementById("fullimg");
 
-function openFullImg(reference){
-    fullImgBox.style.display="flex";
-    fullimg.src=reference;
+function openFullImg(reference,valor){
+    if(valor){
+        fullImgBox.style.display="flex";
+        fullimg.src=reference;
+    }else{
+        fullImgBox.style.display="flex";
+        fullimg.src=reference;
+        fullimg.style.width=400;
+        fullimg.style.height=auto;
+    }
+    
 }
 
 function closeImg(){
