a8121728048b6c04b5b71ab8b5521f4bedca8f40
Fer-Mig-Agus <miguelfernandez.m.f.8998@gmail.com>
2022-08-26 21:25:06 -0300

    Main: He realizado la documentacion de todos los archivos de la carpeta main

--- main/js/app.js
+++ main/js/app.js
@@ -1,6 +1,9 @@
+
+//Estas son variables para la imagen que se abre en otra ventana
 const fullImgBox=document.getElementById("fullImgBox");
 const fullimg=document.getElementById("fullimg");
 
+//Esta funcion abre y modifica el tamaño de la imagen, dependiendo si es vertical u Horizontal
 function openFullImg(reference,valor){
     if(valor){
         fullImgBox.style.display="flex";
@@ -16,22 +19,23 @@ function openFullImg(reference,valor){
     }
     
 }
+//Cierra la ventana en donde se muestra las imagenes
 
 function closeImg(){
     fullImgBox.style.display="none";
 
 }
 
+//Estas variables son para el boton ver mas
 const fullImgVert=document.querySelector('.bottom-verMas');
 const masInfo=document.querySelector('#mas-info-tarjetas');
 const divMore=document.querySelector('#imgMore');
 const divNosotros=document.querySelector('#nosotros');
 const cuerpoPrincipal=document.querySelector('#cuerpo');
-
+//este es un evento click, que llama a la funcion ver mas
 fullImgVert.addEventListener('click',funcionVerMas);
-
-console.log(divMore.style.display.contains());
-
+//Funcion ver mas que modifica el tamaño de la seccion del cuerpo
+//para que se muestren mas imagenes de los trabajos realizados
 function funcionVerMas(){
     if(divMore.classList.contains('activo')){
         divMore.classList.remove('activo');
